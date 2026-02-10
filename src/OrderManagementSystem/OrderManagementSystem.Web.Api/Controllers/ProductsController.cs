f044145236d0cb7b5d8f1887ac4d385b38483e26
EmreWolf58 <emrekurt14070@gmail.com>
2026-02-11 02:23:36 +0300

    feat: add EF Core

---
+++ src/OrderManagementSystem/OrderManagementSystem.Web.Api/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderManagementSystem.Infrastructure.Data;
+using OrderManagementSystem.Core.Entities;
+
+namespace OrderManagementSystem.Web.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ProductsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            _context.Products.Add(product);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new {id=product.Id}, product);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById (int id)
+        {
+            var product = _context.Products.Find(id);
+            if (product == null) return NotFound();
+            return Ok(product);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(_context.Products.ToList());
+        }
+    }
+}

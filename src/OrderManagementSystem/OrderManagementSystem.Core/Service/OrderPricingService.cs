e561bd90ff854ef0d30227d5c7e7de9aa1eaa893
EmreWolf58 <emrekurt14070@gmail.com>
2026-02-18 01:51:57 +0300

    add unit test

---
+++ src/OrderManagementSystem/OrderManagementSystem.Core/Service/OrderPricingService.cs
@@ -0,0 +1,24 @@
+﻿using OrderManagementSystem.Core.Dtos;
+
+namespace OrderManagementSystem.Core.Services
+{
+    public class OrderPricingService
+    {
+        public decimal CalculateTotal(IEnumerable<OrderLineDto> lines)
+        {
+            if (lines == null) throw new ArgumentNullException(nameof(lines));
+
+            decimal total = 0;
+
+            foreach (var l in lines)
+            {
+                if (l.Quantity <= 0) throw new ArgumentException("Quantity must be > 0");
+                if (l.UnitPrice < 0) throw new ArgumentException("UnitPrice must be >= 0");
+
+                total += l.UnitPrice * l.Quantity;
+            }
+
+            return total;
+        }
+    }
+}

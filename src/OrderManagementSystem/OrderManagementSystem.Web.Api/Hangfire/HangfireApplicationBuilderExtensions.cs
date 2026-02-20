89f2ef9cc0a3ea28cbfcee45a507c9b6b3fde4f2
EmreWolf58 <emrekurt14070@gmail.com>
2026-02-21 01:08:54 +0300

    add docker and hangfire

---
+++ src/OrderManagementSystem/OrderManagementSystem.Web.Api/Hangfire/HangfireApplicationBuilderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using Hangfire;
+using Hangfire.Dashboard;
+using OrderManagementSystem.Infrastructure.Jobs;
+
+namespace OrderManagementSystem.Web.Api.Hangfire;
+
+public static class HangfireApplicationBuilderExtensions
+{
+    public static IApplicationBuilder UseHangfireSetup(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        // Dashboard (dev/prod stratejini burada yönet)
+        if (env.IsDevelopment())
+        {
+            app.UseHangfireDashboard("/hangfire");
+        }
+        else
+        {
+            app.UseHangfireDashboard("/hangfire", new DashboardOptions
+            {
+                Authorization = new[] { new HangfireDashboardAuthorizationFilter() }
+            });
+        }
+
+        // Recurring job'lar (3. adım: schedule)
+        RecurringJob.AddOrUpdate<ReportJobService>(
+            "daily-report",
+            x => x.DailyReport(),
+            "0 1 * * *" // her gün 01:00
+        );
+
+        return app;
+    }
+}

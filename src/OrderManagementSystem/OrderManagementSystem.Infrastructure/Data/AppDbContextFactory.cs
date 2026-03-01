8ee9e2cabb89caad0a60b68683b3ead37fea706c
EmreWolf58 <emrekurt14070@gmail.com>
2026-03-01 15:28:02 +0300

    Add ,RabbitMQ

---
+++ src/OrderManagementSystem/OrderManagementSystem.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace OrderManagementSystem.Infrastructure.Data;
+
+public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+{
+    public AppDbContext CreateDbContext(string[] args)
+    {
+        // Migration çalışırken config’i buradan okuyacağız
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            // Paket Manager Console genelde startup projesinin klasöründen çalışır
+            // Bu yüzden appsettings.json'un doğru path’ini vermek gerekebilir.
+            .AddJsonFile("appsettings.json", optional: true)
+            .AddJsonFile("src/OrderManagementSystem.Web.Api/appsettings.json", optional: true)
+            .AddJsonFile("OrderManagementSystem.Web.Api/appsettings.json", optional: true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var connStr = configuration.GetConnectionString("DefaultConnection")
+                     ?? throw new InvalidOperationException("ConnectionStrings:DefaultConnection bulunamadı.");
+
+        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+        optionsBuilder.UseSqlServer(connStr);
+
+        return new AppDbContext(optionsBuilder.Options);
+    }
+}
---
+++ src/OrderManagementSystem/OrderManagementSystem.Infrastructure/Migrations/20260226193458_AddCustomerEmailToOrders.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrderManagementSystem.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCustomerEmailToOrders : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Customername",
+                table: "Orders",
+                newName: "CustomerName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerEmail",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CustomerEmail",
+                table: "Orders");
+
+            migrationBuilder.RenameColumn(
+                name: "CustomerName",
+                table: "Orders",
+                newName: "Customername");
+        }
+    }
+}
